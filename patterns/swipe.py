509bc8bed88606ff219f9268c4e96b4600841a48
Paul Brook <paul@nowt.org>
2014-07-03 15:20:04 +0100

    Swipe in both directions.
    Signed-off-by: Paul Brook <paul@nowt.org>

--- patterns/swipe.py
+++ patterns/swipe.py
@@ -7,9 +7,15 @@ class Pattern(object):
 
     def init(self):
         self.phase = 0
-        self.offset = 0
-        return 0.2
+        self.offset = -self.cube.size
+        return 1.0 / self.cube.size
     def tick(self):
+        self.offset += 1
+        if self.offset == self.cube.size:
+            self.offset = 1 - self.cube.size
+            self.phase += 1
+            if self.phase == 3:
+                raise StopIteration;
         p0 = self.phase
         p1 = (p0 + 1) % 3
         p2 = (p1 + 1) % 3
@@ -18,7 +24,7 @@ class Pattern(object):
         on = [0]*3
         on[p0] = 255
         for i in range(0, self.cube.size):
-            if i == self.offset:
+            if i == self.cube.size - (abs(self.offset) + 1):
                 color = on
             else:
                 color = off
@@ -28,9 +34,3 @@ class Pattern(object):
                 for k in range(0, self.cube.size):
                     pos[p2] = k
                     self.cube.set_pixel(pos, color)
-        self.offset += 1
-        if self.offset == self.cube.size:
-            self.offset = 0
-            self.phase += 1
-            if self.phase == 3:
-                raise StopIteration;
