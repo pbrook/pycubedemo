0f78257936852725aa65cc527c8fd552b00392a9
Paul Brook <paul@nowt.org>
2014-07-03 14:26:22 +0100

    Initial checkin
    Signed-off-by: Paul Brook <paul@nowt.org>

---
+++ patterns/swipe.py
@@ -0,0 +1,36 @@
+# Swipe a plane of pixels accross the cube
+
+class Pattern(object):
+    def __init__(self, cube):
+        self.cube = cube
+        self.name = "swipe"
+
+    def init(self):
+        self.phase = 0
+        self.offset = 0
+        return 0.2
+    def tick(self):
+        p0 = self.phase
+        p1 = (p0 + 1) % 3
+        p2 = (p1 + 1) % 3
+        pos = [0]*3
+        off = [0]*3
+        on = [0]*3
+        on[p0] = 255
+        for i in range(0, self.cube.size):
+            if i == self.offset:
+                color = on
+            else:
+                color = off
+            pos[p0] = i
+            for j in range(0, self.cube.size):
+                pos[p1] = j
+                for k in range(0, 8):
+                    pos[p2] = k
+                    self.cube.set_pixel(pos, color)
+        self.offset += 1
+        if self.offset == self.cube.size:
+            self.offset = 0
+            self.phase += 1
+            if self.phase == 3:
+                raise StopIteration;
---
+++ pixel.scad
@@ -0,0 +1,1 @@
+cylinder(r1=0.5, r2=0.3, h=1, center=true, $fn=20);
