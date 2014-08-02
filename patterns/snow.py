cb283c002193c1acb6d7cf1746e2038fe81bb1b4
Rahim Packir Saibo <i.am@rah.im>
2014-08-03 00:12:15 +0100

    Added 'snow' pattern - a slightly reworked rain, slower, pastels, interpolation

---
+++ patterns/snow.py
@@ -0,0 +1,66 @@
+# Fork of rain - pastel "snow flakes" slowly falling (interpolated)
+# Copyright (C) Paul Brook <paul@nowt.org>
+# Released under the terms of the GNU General Public License version 3
+
+import random
+import math
+import cubehelper
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+SPAWN_PROBABILITY = 0.5
+
+class Drop(object):
+    def __init__(self, cube, x, y):
+        self.cube = cube
+        self.x = x
+        self.y = y
+        self.z = -1
+    def reset(self):
+        self.z = self.cube.size
+        self.speed = random.uniform(0.2, 0.1)
+        self.color = cubehelper.mix_color(cubehelper.random_color(), WHITE, 0.9)
+    def tick(self):
+        self.z -= self.speed
+        if self.z >= 0:
+            self.set_pixel_interpolate_float_z(self.x, self.y, self.z, self.color)
+    # TODO: generalize interpolation, place in cubehelper
+    # assumes z > 0
+    def set_pixel_interpolate_float_z(self, x, y, z, color):
+        # we're always on a pixel, or between two
+        if float(int(z)) == z: # on pixel
+            self.cube.set_pixel((x, y, int(z)), color)
+        else: # between
+            z0 = int(math.floor(z))
+            z1 = int(math.floor(z+1))
+            z1_strength = z - z0
+            z0_strength = 1 - z1_strength
+
+            self.cube.set_pixel((x, y, z0), cubehelper.mix_color(BLACK, color, z0_strength))
+            self.cube.set_pixel((x, y, z1), cubehelper.mix_color(BLACK, color, z1_strength))
+
+class Pattern(object):
+    def init(self):
+        self.double_buffer = True
+        self.drops = []
+        self.unused = []
+        for x in range(0, self.cube.size):
+            for y in range(0, self.cube.size):
+                self.unused.append(Drop(self.cube, x, y))
+        return 0.1
+    def spawn(self):
+        d = self.unused.pop(random.randrange(len(self.unused)))
+        d.reset()
+        self.drops.append(d)
+    def tick(self):
+        if random.random() < SPAWN_PROBABILITY:
+            self.spawn()
+        drops = self.drops;
+        self.drops = []
+        self.cube.clear()
+        for d in drops:
+            d.tick()
+            if d.z < 0:
+                self.unused.append(d)
+            else:
+                self.drops.append(d)
