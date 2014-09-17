b6df1af8ba9e0b16e0cf017103baae00bc853be4
Alex Silcock <alex@alexsilcock.net>
2014-09-17 15:08:54 +0100

    Show lights from all faces on cube. Currently just adds colours (need to make this smarter)

--- patterns/gravityblocks.py
+++ patterns/gravityblocks.py
@@ -5,6 +5,7 @@ import json
 import thread
 import time
 import copy
+import numpy
 from threading import Lock
 from collections import defaultdict
 
@@ -22,16 +23,16 @@ class Pattern(object):
 
         # Define some queues of empty planes. Each face of the cube has a full cube's worth of planes, which are merged
         # together when rendered
-        black = (0.0, 0.0, 0.0)
+        black = (0, 0, 0)
         sz = self.cube.size
 
         self.facePlanes = {
-            'top': QueueList(self.cube.size, [[black for x in range(0, sz)] for y in range(0, sz)]),
-            'front': QueueList(self.cube.size, [[black for x in range(0, sz)] for y in range(0, sz)]),
-            'left': QueueList(self.cube.size, [[black for x in range(0, sz)] for y in range(0, sz)]),
-            'right': QueueList(self.cube.size, [[black for x in range(0, sz)] for y in range(0, sz)]),
-            'back': QueueList(self.cube.size, [[black for x in range(0, sz)] for y in range(0, sz)]),
-            'bottom': QueueList(self.cube.size, [[black for x in range(0, sz)] for y in range(0, sz)])
+            'top': FaceHistory(self.cube.size, 'top', [[black for x in range(0, sz)] for y in range(0, sz)]),
+            'front': FaceHistory(self.cube.size, 'front', [[black for x in range(0, sz)] for y in range(0, sz)]),
+            'left': FaceHistory(self.cube.size, 'left', [[black for x in range(0, sz)] for y in range(0, sz)]),
+            'right': FaceHistory(self.cube.size, 'right', [[black for x in range(0, sz)] for y in range(0, sz)]),
+            'back': FaceHistory(self.cube.size, 'back', [[black for x in range(0, sz)] for y in range(0, sz)]),
+            'bottom': FaceHistory(self.cube.size, 'bottom', [[black for x in range(0, sz)] for y in range(0, sz)])
         }
 
         self.colors = [] # set when the welcome message is received
@@ -59,10 +60,29 @@ class Pattern(object):
 
         # render planes to cube
         # TODO: make this merge all planes. Currently just renders front plane
-        for y, plane in enumerate(self.facePlanes['front']):
-            for x, row in enumerate(plane):
-                for z, color in enumerate(row):
-                    self.cube.set_pixel((x, y, z), color)
+        merged = self.merge_planes(self.facePlanes)
+        for y in range(0, self.cube.size):
+            for x in range(0, self.cube.size):
+                for z in range(0, self.cube.size):
+                    self.cube.set_pixel((x, y, z), cubehelper.color_to_float(merged[y][x][z]))
+
+    def merge_planes(self, faces):
+        """Take an array of faces, each of which is an array of planes, and merge them into one 3D array of pixels, taking into
+        account the view perspective of the original faces. This sounds mad, but yes, this does mean that each face has an
+        entire cube's worth of pixels. I'll probably change this to use a particle system at some point."""
+
+        # Array to store merged colours in. 3D array with a final dimension for the R, G, B components.
+        computed = numpy.empty([self.cube.size, self.cube.size, self.cube.size, 3], dtype=float)
+
+        for x in range(self.cube.size):
+            for y in range(self.cube.size):
+                for z in range(self.cube.size):
+                    # Index the array with y coord at the start, to optimise outputting serial data to the cube
+                    faceColours = [f.get_from_front_perspective(x, y, z) for f in self.facePlanes]
+                    computed[y][x][z] = numpy.sum(faceColours, axis=0) # TODO do something more intelligent to merge colours rather than just adding them, which will end in tears
+
+        return computed
+
 
     def on_open(self):
         print("Connected to GravityBlocks server")
@@ -77,12 +97,12 @@ class Pattern(object):
 
     def on_activate(self, data):
         with self.pixels_lock:
-            col = cubehelper.color_to_float(self.colors[data["color"]])
+            col = tuple(self.colors[data["color"]])
             self.pixels_to_set[data["face"]].append((self.translate_coords(data["coords"]["x"], data["coords"]["y"]), col))
 
     def on_deactivate(self, data):
         with self.pixels_lock:
-            self.pixels_to_set[data["face"]].append((self.translate_coords(data["coords"]["x"], data["coords"]["y"]), (0.0, 0.0, 0.0)))
+            self.pixels_to_set[data["face"]].append((self.translate_coords(data["coords"]["x"], data["coords"]["y"]), (0, 0, 0)))
 
     def translate_coords(self, x, y):
         """ Translate finger-coordinates (i.e. top left is 0, 0, bottom left is 0, 7) to cube front face coords """
@@ -138,10 +158,14 @@ class EventedWebsocket(object):
     def on_close(self, ws):
         self.run_handlers("close")
 
-class QueueList(object):
-    """Similar to a simplified deque but with O(1) random access instead of O(n)"""
-    def __init__(self, size, initVal):
+class FaceHistory(object):
+    """Similar to a simplified deque but with O(1) random access instead of O(n). Represents
+    a 3D cube made up of planes, each of which is an item in a queue. New planes can be pushed
+    (prepended) to the front of the queue, pushing old items off the end."""
+
+    def __init__(self, size, face, initVal):
         self.size = size
+        self.face = face
         self.contents = [initVal] * size
         self.head_pointer = 0
 
@@ -150,6 +174,26 @@ class QueueList(object):
         new_head_pointer = self._wrap_into_valid_range(self.head_pointer - 1)
         self.head_pointer = new_head_pointer
         self.contents[new_head_pointer] = item
+
+    def get_from_front_perspective(self, x, y, z):
+        """Get an item from the cube, with the coordinates passed in from the front of the cube's perspective."""
+        translated = self._translate_from_front_to(self.face, x, y, z)
+        return self[translated[0]][translated[1]][translated[2]] 
+
+    def _translate_from_front_to(self, perspective, x, y, z):
+        """Translate a coordinate passed in from the perspective of the front of the cube to the specified perspective"""
+        if self.face == "front":
+            return [x, y, z]
+        elif self.face == "back":
+            return [8-x, 8-y, z]
+        elif self.face == "left":
+            return [8-y, x, z]
+        elif self.face == "right":
+            return [y, 8-x, z]
+        elif self.face == "top":
+            return [x, 8-z, y]
+        elif self.face == "bottom":
+            return [x, z, 8-y]
 
     def _wrap_into_valid_range(self, pointer):
         """Take a pointer index and return it mapped into the valid range for the queue"""
