7ed745224b6b2f6ea9ba2de07c5eb2d32ab6aeb8
Paul Brook <paul@nowt.org>
2014-09-30 19:46:32 +0100

    Fade plasma in and out
    Signed-off-by: Paul Brook <paul@nowt.org>

--- patterns/plasma.py
+++ patterns/plasma.py
@@ -6,7 +6,9 @@ import random
 import cubehelper
 import math
 
-DT = 0.05
+DT = 1.0/20
+FADE_TIME = 5.0
+ACTIVE_TIME = 5.0
 
 def color_from_val(val):
     if val < 85:
@@ -26,16 +28,23 @@ def color_from_val(val):
 class Pattern(object):
     def init(self):
         self.offset = 0.0
+        self.fade_distance = 0.0
+        self.timer = 0.0
         return DT
 
     def tick(self):
+        self.timer += DT
         self.offset -= DT / 1.0
         if self.offset < 0:
             self.offset += 1.0
-            raise StopIteration
         sz = self.cube.size
         scale = math.pi * 2.0 / float(sz)
         offset = 0.5
+        if self.timer < FADE_TIME:
+            self.fade_distance += DT * 11.0 / FADE_TIME
+        elif self.timer > FADE_TIME + ACTIVE_TIME:
+            self.fade_distance -= DT * 11.0 / FADE_TIME
+
         for y in range(0, sz):
             for z in range(0, sz):
                 for x in range(0, sz):
@@ -44,4 +53,13 @@ class Pattern(object):
                     w = math.cos((z + offset) * scale)
                     e = (u + v + w + 3.0) / 6.0
                     color = self.cube.plasma(self.offset + e)
+                    dist = abs(x - 3.5) + abs(y - 3.5) + abs(z - 3.5)
+                    delta = dist - self.fade_distance
+                    if delta > 0.0:
+                        if delta > 1.0:
+                            color = 0
+                        else:
+                            color = cubehelper.mix_color(color, 0, delta)
                     self.cube.set_pixel((x, y, z), color)
+        if self.timer > FADE_TIME * 2.0 + ACTIVE_TIME:
+            raise StopIteration
